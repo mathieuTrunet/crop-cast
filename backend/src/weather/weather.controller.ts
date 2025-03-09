a151136aaa80e71838489b0f7c886eec6381fd08
mathieuTrunet <mathieu1508@gmail.com>
2025-03-09 16:36:51 +0100

    feature: add validation to weather controller

--- backend/src/weather/weather.controller.ts
+++ backend/src/weather/weather.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common'
+import { Controller, Get, Query, ValidationPipe } from '@nestjs/common'
 import { WeatherService } from './weather.service'
 import { GetWeatherDto } from './dto/read-weather.dto'
 
@@ -7,27 +7,27 @@ export class WeatherController {
   constructor(private readonly weatherService: WeatherService) {}
 
   @Get()
-  getWeatherData(@Query() getWeatherDto: GetWeatherDto) {
+  getWeatherData(@Query(new ValidationPipe({ transform: true })) getWeatherDto: GetWeatherDto) {
     return this.weatherService.getWeatherData(getWeatherDto.latitude, getWeatherDto.longitude)
   }
 
   @Get('general')
-  getWeatherGeneral(@Query() getWeatherDto: GetWeatherDto) {
+  getWeatherGeneral(@Query(new ValidationPipe({ transform: true })) getWeatherDto: GetWeatherDto) {
     return this.weatherService.getWeatherGeneral(getWeatherDto.latitude, getWeatherDto.longitude)
   }
 
   @Get('temperature')
-  getTemperature(@Query() getWeatherDto: GetWeatherDto) {
+  getTemperature(@Query(new ValidationPipe({ transform: true })) getWeatherDto: GetWeatherDto) {
     return this.weatherService.getTemperature(getWeatherDto.latitude, getWeatherDto.longitude)
   }
 
   @Get('humidity')
-  getHumidity(@Query() getWeatherDto: GetWeatherDto) {
+  getHumidity(@Query(new ValidationPipe({ transform: true })) getWeatherDto: GetWeatherDto) {
     return this.weatherService.getHumidity(getWeatherDto.latitude, getWeatherDto.longitude)
   }
 
   @Get('light-exposure')
-  getLightExposure(@Query() getWeatherDto: GetWeatherDto) {
+  getLightExposure(@Query(new ValidationPipe({ transform: true })) getWeatherDto: GetWeatherDto) {
     return this.weatherService.getLightExposure(getWeatherDto.latitude, getWeatherDto.longitude)
   }
 }
