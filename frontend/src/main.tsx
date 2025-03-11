3f1c395b7c59943868d557ddf766f576c2da7372
mathieuTrunet <mathieu1508@gmail.com>
2025-03-11 13:24:57 +0100

    feat: added weather context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,13 +5,16 @@ import MainPage from './pages/MainPage'
 import { LocationProvider } from './lib/context/LocationContext'
 import { TimeProvider } from './lib/context/TimeContext'
 import { QueryProvider } from './lib/context/QueryProvider'
+import { WeatherProvider } from './lib/context/WeatherContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryProvider>
       <LocationProvider>
         <TimeProvider>
-          <MainPage />
+          <WeatherProvider>
+            <MainPage />
+          </WeatherProvider>
         </TimeProvider>
       </LocationProvider>
     </QueryProvider>
